6fb814f9848d4448cd40a074f2df6b9b98d8f710
Peter Beňo <najlepsiwebdesigner@gmail.com>
2015-12-02 10:25:53 +0100

    add PGSQL on mac os x, rename to SciDB

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -1,10 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <pqxx/pqxx>
+#include <iostream>
+
+using namespace pqxx;
+using namespace std;
+
 MainWindow::MainWindow(QWidget *parent)
 : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    try{
+          connection C("dbname=scidb user=postgres password=postgres \
+          hostaddr=127.0.0.1 port=5432");
+          if (C.is_open()) {
+             cout << "Opened database successfully: " << C.dbname() << endl;
+          } else {
+             cout << "Can't open database" << endl;
+//             return 1;
+          }
+          C.disconnect ();
+       }catch (const std::exception &e){
+          cerr << e.what() << std::endl;
+//          return 1;
+       }
+
+
+
 }
 
 MainWindow::~MainWindow()
